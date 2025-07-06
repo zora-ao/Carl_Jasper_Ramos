fe1d002ceba32dc46be8ddf3e8b39101feb2dda0
Carl Jasper Ramos <carljasperramos97@gmail.com>
2025-07-06 14:34:57 +0800

    changes

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const certificatesImg = [
-    {title: "Web Design", src: './certificates/responsive.png'},
-    {title: "Javascript Data Structure and Algorithm", src: './certificates/javascript.png'},
-    {title: "Front End Development Libraries", src: './certificates/react.png'}
+    {title: "Web Design", src: './certificates/responsive.png', link: "https://www.freecodecamp.org/certification/ao_zora/responsive-web-design"},
+    {title: "Javascript Data Structure and Algorithm", src: './certificates/javascript.png', link: "https://www.freecodecamp.org/certification/ao_zora/javascript-algorithms-and-data-structures-v8"},
+    {title: "Front End Development Libraries", src: './certificates/react.png', link: "https://www.freecodecamp.org/certification/ao_zora/front-end-development-libraries"}
 ];
 
 const certificateBtn = document.querySelectorAll('.certificate-btn');
 const displayCertificate = document.getElementById('display-certificate');
+const closeCertificate = document.getElementById("close-certificate");
+const certificateImg = document.getElementById("certificate-img");
+const certificateLink = document.getElementById('certificate-link');
 
 certificateBtn.forEach((btn, index) => {
     btn.addEventListener('click', () => {
@@ -15,31 +18,42 @@ certificateBtn.forEach((btn, index) => {
         setTimeout(() => {
             displayCertificate.setAttribute('src', certificatesImg[index].src);
             displayCertificate.classList.remove('opacity-0');
+            certificateImg.classList.toggle('hidden');
+            certificateLink.setAttribute(href, certificatesImg[index].link)
         }, 500)  
     })
 });
 
+closeCertificate.addEventListener('click', () => {
+    certificateImg.classList.toggle('hidden');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+//Transitions
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if(entry.isIntersecting){
-            if(
-                entry.target.classList.contains('hide')
-            ){
+            if(entry.target.classList.contains('hide')){
                 entry.target.classList.remove('hide')
                 entry.target.classList.add('show')
                 
             }
-
-            if(entry.target.classList.contains('timeline')){
-                entry.target.classList.add('animate')
-            }
         } else {
             entry.target.classList.remove('show')
-            entry.target.classList.remove('animate')
             entry.target.classList.add('hide')
         }
     })
-})
+});
 
-const element = document.querySelectorAll('.timeline, .hide');
+const element = document.querySelectorAll('.hide');
 element.forEach((el) => observer.observe(el));
